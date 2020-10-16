9939a843f81f32299d8f242fc6fc368ffae4ab90
Riikka Sutinen <riikka.e.sutinen@gmail.com>
2020-10-16 18:25:13 +0300

    2.7 tehtävän palautus

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+      event.preventDefault()
+      const nameObject = {
+          name: newName
+      }
+
+      const nameExists = persons.some(person => person.name === newName)
+
+      if (nameExists) {
+          window.alert(`${newName} is already in the phonebook.`)
+      } else {
+      setPersons(persons.concat(nameObject))
+      setNewName('') }
+  }
+
+  const handleNameChange = (event) => {
+      console.log(event.target.value)
+      setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+    </div>
+  )
+
+}
+
+export default App
