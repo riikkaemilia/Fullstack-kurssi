74c04543561869641f4c4433399c360ce971aa6b
Riikka Sutinen <riikka.e.sutinen@gmail.com>
2020-11-01 12:30:54 +0200

    Kurssitietoihin pieni muokkaus
    Siirretty mappaus Part-komponentista Content-komponenttiin. Sisennykset kuntoon.

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,52 +1,52 @@
 import React from 'react'
 
-const Total = ({course}) => {
-//    console.log('total props are', course)
-    return (
-      <b><p>
-        Total is {course.reduce(function(sum,part) {
-        return sum + part.exercises
-      }, 0)} exercises!
+const Total = ({ course }) => {
+  //    console.log('total props are', course)
+  return (
+    <b><p>
+      Total is {course.reduce(function (sum, part) {
+      return sum + part.exercises
+    }, 0)} exercises!
       </p></b>
-    )
-  }
-  
-  const Part = ({parts}) => {
-//    console.log('part props are',parts)
-    return (
-      <div>
-      {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-      </div>
-    )
-  }
-  
-  const Content = ({course}) => {
-//    console.log('content props are', course)
-    return (
-  <div>
-  <Part parts={course}/>
-  </div>
-    )
-  }
-  
-  const Header = ({course}) => {
-//    console.log('header props are', course)
-    return (
-        <h2>
-          {course.name}
-        </h2>
-    )
-  }
-  
-  const Course = ({courses}) => {
-//    console.log('Course props are', courses)
-    return (
-        <div>
-        <Header course={courses}/>
-        <Content course={courses.parts}/>
-        <Total course={courses.parts}/>
-        </div>
-      )
-  }
+  )
+}
+
+const Part = ({ parts }) => {
+  // console.log('part props are',parts)
+  return (
+    <p>{parts.name} {parts.exercises}</p>
+  )
+}
+
+const Content = ({ course }) => {
+  // console.log('content props are', course)
+  return (
+    <div>
+      {course.map(part =>
+        <Part key={part.id} parts={part} />
+      )}
+    </div>
+  )
+}
+
+const Header = ({ course }) => {
+  //  console.log('header props are', course)
+  return (
+    <h2>
+      {course.name}
+    </h2>
+  )
+}
+
+const Course = ({ courses }) => {
+  //  console.log('Course props are', courses)
+  return (
+    <div>
+      <Header course={courses} />
+      <Content course={courses.parts} />
+      <Total course={courses.parts} />
+    </div>
+  )
+}
 
 export default Course
--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -5,76 +5,76 @@ import Course from './components/Course'
 const App = () => {
   const courses = [
     {
-  name: 'Half Stack application development',
-  id: 1,
-  parts: [ 
-  {
-    name: 'Fundamentals of React',
-    exercises: 10,
-    id: 1
-  },
-  { 
-    name: 'Using props to pass data',
-    exercises: 7,
-    id: 2
-  },
-  {
-    name: 'State of a component',
-    exercises: 14,
-    id: 3
-  },
-  {
-    name: 'Modules',
-    exercises: 8,
-    id: 4
-  },
-  {
-    name: 'Very cool stuff',
-    exercises: 65,
-    id: 5
-  }
-]
-},
-{
-  name: 'Node.js',
-  id: 2,
-  parts: [
-    {
-      name: 'Routing',
-      exercises: 3,
-      id: 1
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Modules',
+          exercises: 8,
+          id: 4
+        },
+        {
+          name: 'Very cool stuff',
+          exercises: 65,
+          id: 5
+        }
+      ]
     },
     {
-      name: 'Middlewares',
-      exercises: 7,
-      id: 2
-    }
-  ]
-},
-{
-  name: 'Absolute magic',
-  id: 3,
-  parts: [
-    {
-      name: 'Maps are awesome',
-      exercises: 7,
-      id: 1
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     },
     {
-      name: 'Reduce is pretty cool too',
-      exercises: 18,
-      id: 2
+      name: 'Absolute magic',
+      id: 3,
+      parts: [
+        {
+          name: 'Maps are awesome',
+          exercises: 7,
+          id: 1
+        },
+        {
+          name: 'Reduce is pretty cool too',
+          exercises: 18,
+          id: 2
+        }
+      ]
     }
   ]
-}
-]
 
-// console.log('app contains array', courses)
+  // console.log('app contains array', courses)
 
   return (
     <div>
       {courses.map(course =>
-      <Course key={course.id} courses={course} />
+        <Course key={course.id} courses={course} />
       )}
     </div>
   )
