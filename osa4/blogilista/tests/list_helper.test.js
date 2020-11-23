098942d494562567a3dc5147b47fe9d32994bc60
Riikka Emilia <riikka.e.sutinen@gmail.com>
2020-11-23 22:32:39 +0200

    Tehtävän 4.6 palautus

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -15,37 +15,30 @@ const listWithOneBlog = [
 ];
 
 const blogWithMostLikes = {
-  _id: "5a422b3a1b54a676234d17f9",
-  title: "Canonical string reduction",
+  title: "Go To Statement Considered Harmful",
   author: "Edsger W. Dijkstra",
-  url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
   likes: 12,
-  __v: 0,
+};
+
+const mostBlogs = {
+  author: "Robert C. Martin",
+  blogs: 3,
 };
 
 const blogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url:
-      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 12,
-    __v: 0,
-  },
   {
     _id: "5a422b3a1b54a676234d17f9",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
     likes: 12,
     __v: 0,
   },
@@ -56,6 +49,15 @@ const blogs = [
     url:
       "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 12,
     __v: 0,
   },
   {
@@ -100,9 +102,16 @@ describe("total likes", () => {
 });
 
 describe("favorite blog", () => {
-  test("blog with the most likes", () => {
+  test("return blog with most likes", () => {
     const result = listHelper.favoriteBlog(blogs);
-    console.log(result);
     expect(result).toEqual(blogWithMostLikes);
+  });
+});
+
+describe("most blogs", () => {
+  test("return author with most blogs", () => {
+    const result = listHelper.mostBlogs(blogs);
+    console.log(result);
+    expect(result).toEqual(mostBlogs);
   });
 });
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -12,11 +14,27 @@ const favoriteBlog = (blogs) => {
   const mostLikes = blogs.reduce((accumulator, currentValue) =>
     accumulator.likes > currentValue.likes ? accumulator : currentValue
   );
-  return mostLikes;
+  return {
+    title: mostLikes.title,
+    author: mostLikes.author,
+    likes: mostLikes.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  const authorsBlogs = _(blogs)
+    // Group the elements of Array based on author property
+    .groupBy("author")
+    // `key` is group's name (author), `value` is the array of objects
+    .map((value, key) => ({ author: key, blogs: value.length }))
+    .value();
+
+  return _.maxBy(authorsBlogs, "blogs");
 };
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
