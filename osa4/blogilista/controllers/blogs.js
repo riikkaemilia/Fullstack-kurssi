c441b14b85662af5d0b07b6636416485ab3fa413
Riikka Emilia <riikka.e.sutinen@gmail.com>
2020-11-29 21:00:40 +0200

    Tehtävän 4.14 palautus

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -32,4 +32,37 @@ blogsRouter.post("/", async (request, response) => {
   response.json(savedBlog.toJSON());
 });
 
+// Blogin poistaminen
+blogsRouter.delete("/:id", async (request, response) => {
+  const blog = await Blog.findByIdAndRemove(request.params.id);
+  if (blog) {
+    response.status(204).end();
+  } else {
+    response.status(404).end();
+  }
+});
+
+// Blogin muokkaaminen
+blogsRouter.put("/:id", async (request, response) => {
+  const { body } = request;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  });
+  if (updatedBlog === null) {
+    response.status(404).end();
+  } else {
+    response.json(updatedBlog.toJSON());
+  }
+});
+
 module.exports = blogsRouter;
